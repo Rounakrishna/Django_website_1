8d4485f9ffe14a2649c8da881b90a4869ab4fe2a
Krishna Kumar <93572124+Rounakrishna@users.noreply.github.com>
2024-06-29 15:40:08 +0530

    Add files via upload

---
+++ lms/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect,render
+
+def BASE(request):
+    return render(request,'base.html')
+
+def HOME(request):
+    return render(request,'Main/home.html')
+
+def SINGLE_COURSE(request):
+    return render(request,'Main/single_course.html')
+
+def CONTACT(request):
+    return render(request,'Main/contact.html')
+
+def ABOUT(request):
+    return render(request,'Main/about.html')
